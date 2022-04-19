55f715e255344649d253ffe36788fb9d00784233
Austin Dye <thedyeguy1996@outlook.com>
2022-04-18 22:02:07 -0600

    uhhhhh

--- BurgerData/server/controllers/BorgersController.js
+++ BurgerData/server/controllers/BorgersController.js
@@ -2,5 +2,7 @@ import BaseController from "../utils/BaseController";
 
 
 export class BorgersController extends BaseController {
-    
+    constructor(){
+        super('https://4bca9122-01c6-4eb2-a32b-bfad3aeb2d4c.mock.pstmn.io/borgers') //???
+    }
 }
